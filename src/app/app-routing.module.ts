746dd24556c86a513d5060a0b8fedf133a12508f
certyfreak <certifiedfreak2001@gmail.com>
2022-02-02 10:05:46 +0000

    updating layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomComponent } from './components/custom/custom.component';
 import { HomeComponent } from './components/home/home.component';
+import { InfoComponent } from './components/info/info.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { SettingComponent } from './components/setting/setting.component';
 import { SupportComponent } from './components/support/support.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'support',component:SupportComponent},
+  {path:'custom',component:CustomComponent},
+  {path:'progress',component:ProgressComponent },
+  {path:'setting',component:SettingComponent},
+  {path:'info',component:InfoComponent},
   {path:' ',component:HomeComponent},
   {path:'**',component:HomeComponent}
 ];
