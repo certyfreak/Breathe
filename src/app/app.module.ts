406f980ebbb3bd9341e4bf6db8f0e72b533eeb1a
certyfreak <certifiedfreak2001@gmail.com>
2022-02-09 12:45:27 +0000

    rebuilding UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,10 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
-import { SupportComponent } from './components/support/support.component';
-import { SettingComponent } from './components/setting/setting.component';
-import { InfosectionService } from './services/infosection.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SupportComponent,
-    SettingComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +16,7 @@ import { InfosectionService } from './services/infosection.service';
     AppRoutingModule,
     BrowserAnimationsModule,
   ],
-  providers: [InfosectionService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/info/INFO.ts
+++
@@ -1,6 +0,0 @@
-export interface INFO {
-    equal : boolean;
-    square : boolean;
-    mode : boolean;
-    test : boolean
-}
