b45716dcf338ff5295de16e853b934df43bb83f3
Abdul Aziz Ali <azizalialhassan@gmail.com>
2024-07-16 16:54:36 +0000

    add ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CommonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    IonicModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
